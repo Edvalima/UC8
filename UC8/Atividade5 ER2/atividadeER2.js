0d48610d2279cb4e4231b60f639ab78ccb896f8b
Edvá Lima <edvalima25@gmail.com>
2022-09-21 10:00:06 -0300

    3 formas de resolver a atividade

--- UC8/Atividade5 ER2/atividadeER2.js
+++ UC8/Atividade5 ER2/atividadeER2.js
@@ -1,3 +1,6 @@
+//Primeira forma de resolver o exercicio
+
+/*
 let numeroDealunos = 10;
 
 for(let contador = 0; contador <= numeroDealunos; contador++) 
@@ -14,3 +17,45 @@ for(let contador = 0; contador <= numeroDealunos; contador++)
         console.log(`${contador} - IMPAR`)
     }
 }
+*/
+
+//Segunda forma de resolver o exercicio
+/*
+let numeroDealunos = 10;
+let contador = 0
+while(contador <= numeroDealunos)
+{
+
+    if(contador == 0){
+        //console.log("O numero atual é zero")
+        console.log(`${contador} - ZERO`)
+    }
+    else if((contador % 2) == 0){
+        console.log(`${contador} - PAR`)
+    }
+    else {
+        console.log(`${contador} - IMPAR`)
+    }
+    contador++
+}
+*/
+
+// Terceira forma de resolver o exercicio
+
+let numeroDealunos = 10;
+let contador = 0
+do
+{
+
+    if(contador == 0){
+        //console.log("O numero atual é zero")
+        console.log(`${contador} - ZERO`)
+    }
+    else if((contador % 2) == 0){
+        console.log(`${contador} - PAR`)
+    }
+    else {
+        console.log(`${contador} - IMPAR`)
+    }
+    contador++
+}while(contador <= numeroDealunos);
