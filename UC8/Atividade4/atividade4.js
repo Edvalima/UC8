9c694d3d64d815379046b8d4d0c08f7eece2dc9d
Edvá Lima <edvalima25@gmail.com>
2022-09-21 21:09:31 -0300

    Resolução da atividade 4

---
+++ UC8/Atividade4/atividade4.js
@@ -0,0 +1,33 @@
+let data = 21/09/2022
+
+switch (data) {
+    case 21/09/2022:
+        console.log("A data esta correta")
+        break;
+
+default:
+     console.log("A data não é posterior ao evento, então não é possivel continuar com o cadastro.")
+        break;
+}
+
+idade = 18;
+
+if (idade > 20){
+    console.log("Você não possui idade suficiente")
+}
+else{
+    console.log("Idade aceita!")
+}
+
+let lista = ["Edvá","Ednavan","Edvan","Vera"]
+
+lista.push("Renata")
+lista.push("Clara")
+lista.push("Anunciada")
+
+if(lista.length < 100){
+    console.log("Cadastro permitido")
+}
+else{
+    console.log("Cadastro não permitido por ter excedido o limite de pessoas")
+}
